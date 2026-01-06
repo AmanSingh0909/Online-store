9231e181badd3ede36d649cc15f923396c4d6934
Aman Singh <amankumarkvm4@gmail.com>
2026-01-06 21:47:30 +0530

    updated user model

--- user-service/src/models/User.js
+++ user-service/src/models/User.js
@@ -7,10 +7,12 @@ const userSchema = new mongoose.Schema({
     },
     email:{
         type: String,
-        unique: true
+        unique: true,
+        required: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     }
 })
 
