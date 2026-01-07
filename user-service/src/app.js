4475d0e12484d240860937f9edf4cb3cc8bf56d5
Aman Singh <amankumarkvm4@gmail.com>
2026-01-07 21:31:52 +0530

    setup express app and mongodb connection

---
+++ user-service/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect('mongodb://localhost:27017/userdb')
+  .then(() => console.log('User DB connected'))
+  .catch(err => console.error(err));
+
+app.use('/auth', authRoutes);
+
+app.listen(5001, () => {
+  console.log('User Service running on port 5001');
+});
