28fe3a3f2b87fb1fbe5d47a4e73278c06513b23f
Aman Singh <amankumarkvm4@gmail.com>
2026-01-10 23:19:55 +0530

    updated apigate way server file

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -6,7 +6,6 @@ const app = express()
 app.use('/auth', proxy('http://localhost:5001'))
 app.use('/orders', proxy('http://localhost:5003'))
 
-
 app.listen(3000, () => {
     console.log('API Gateway running on 3000')
 })
