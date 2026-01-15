864a4d19eb6dabbc2d96e4d28671808eb7be0381
Aman Singh <amankumarkvm4@gmail.com>
2026-01-15 20:37:11 +0530

    updated post routes of order Routes

--- order-service/src/routes/orderRoutes.js
+++ order-service/src/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const { createOrder } = require('../controller/orderController')
+const authMiddleware = require('../middleware/auth-middleware')
 const router = express.Router()
 
 
-router.post('/', createOrder)
+router.post('/',authMiddleware , createOrder)
 
 module.exports = router
