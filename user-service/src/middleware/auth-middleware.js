842cb8731ae79cab3d8b78002300b81969ef9d6e
Aman Singh <amankumarkvm4@gmail.com>
2026-01-15 20:35:28 +0530

    updated auth middleware

--- user-service/src/middleware/auth-middleware.js
+++ user-service/src/middleware/auth-middleware.js
@@ -10,7 +10,7 @@ const authMiddleware = (req, res, next) => {
             })
         }
 
-        const token = authHeader.split(' ')[1]\
+        const token = authHeader.split(' ')[1]
 
         if (!token) {
             return res.status(401).json({
