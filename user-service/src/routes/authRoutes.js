1e11c26d0c40ba371fe4e5c33b7c8628dfda362a
Aman Singh <amankumarkvm4@gmail.com>
2026-01-18 21:20:33 +0530

    added routes for refresh token and logout

--- user-service/src/routes/authRoutes.js
+++ user-service/src/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const { register, login } = require('../controller/authController')
+const { register, login, refreshToken, logout } = require('../controller/authController')
 const router = express.Router()
 
 router.post('/register', register)
 
 router.post('/login', login)
+
+router.post('/refresh', refreshToken)
+
+router.post('/logout', logout)
 
 module.exports = router
