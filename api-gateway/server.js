fa09807ed70a5a7e6bd1bf41b57d5a1c478000a7
Aman Singh <amankumarkvm4@gmail.com>
2026-01-19 21:15:32 +0530

    added forwordHeader orders routes

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,10 +1,26 @@
-const express = require('express') 
+const express = require('express')
 const proxy = require('express-http-proxy')
+
+const gatewayAuth = require('./middleware/auth-middleware')
 
 const app = express()
 
+app.use('/orders', gatewayAuth);
+
+// Forward user context + traceId to microservices
+const forwardHeaders = (req, proxyReqOpts) => {
+    proxyReqOpts.headers['x-user-id'] = req.user.userId;
+    proxyReqOpts.headers['x-user-role'] = req.user.role;
+
+    return proxyReqOpts;
+};
+
+
 app.use('/auth', proxy('http://localhost:5001'))
-app.use('/orders', proxy('http://localhost:5003'))
+app.use('/orders', proxy('http://localhost:5003', {
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) =>
+        forwardHeaders(srcReq, proxyReqOpts)
+}))
 
 app.listen(3000, () => {
     console.log('API Gateway running on 3000')
